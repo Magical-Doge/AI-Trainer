718fa0571ba823048ee595c1d076851df0ee61c7
魔法狗子 <AvengerSeanCC@qq.com>
2022-02-22 23:29:51 +0800

    some components

---
+++ ABS.py
@@ -0,0 +1,12 @@
+# some components
+
+
+class Abs():
+    def __init__(self):
+        pass
+
+    def abs_goZero(self, cnt, fin, finRe):
+        if cnt != 0:
+            if fin != finRe:
+                cnt = 0
+        return cnt
